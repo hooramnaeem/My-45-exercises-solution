5ca68e41b86dc5f00cb39d8b25771d423f75359f
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:48:05 -0700

    Add files via upload

---
+++ 35_animals.ts
@@ -0,0 +1,13 @@
+// storing names of animals with similler characteristics in a list
+// then printing names of these animals in a loop with statment 
+// at the end of the program printing an additional line in common
+
+let animals:string[]=['Cat','Dog','Horse'];
+for(let pet of animals){
+    console.log('A '+pet+ ' would make a great pet!');
+}
+console.log('Any of these animals can make a great pet!');
+
+
+
+
