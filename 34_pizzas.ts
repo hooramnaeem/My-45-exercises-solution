7f5ce6326fc2ef28b6fb795146f368c069073867
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:46:58 -0700

    Add files via upload

---
+++ 34_pizzas.ts
@@ -0,0 +1,17 @@
+// storing names of pizza in an array then using a for loop for each pizza containing a statment
+// print a sentence along with name of pizzza
+// printing a statment outside the for loop 
+
+let favourit_pizza:string[]=['fajita','rench','tikka']
+for(let pizza of favourit_pizza){
+    console.log('i like '+pizza+ ' so much!');
+
+}
+console.log('I really love pizza!')
+
+
+
+
+
+
+
