4680e5d5a2c87d65e5b1184cd3145c115abe5717
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:29:44 -0700

    Add files via upload

---
+++ 8_four lines_eight.ts
@@ -0,0 +1,9 @@
+//  performing +,_,*,/ and printing number 8 in each line 
+
+console.log(4+4) ;//addition
+
+console.log(16-8);//subtraction
+
+console.log(2*4);//multiplication 
+
+console.log(80/10);//division
