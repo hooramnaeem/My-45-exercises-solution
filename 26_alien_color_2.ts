8fec9ec44b4408f2929304fa6821d38a1acdbab9
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:39:02 -0700

    Add files via upload

---
+++ 26_alien_color_2.ts
@@ -0,0 +1,20 @@
+// writting an if else chain
+// if color is green ,print the statment the player just earned 5 points
+
+
+let Alien_color:string='green';
+if(Alien_color==='green'){
+    console.log("player just earned 5 points!");
+}
+else{
+    console.log("player just earned 10 points!");
+}
+
+// if color is not green print that player just earned 10 points
+Alien_color='yellow';
+if(Alien_color==='green'){
+    console.log("player just earned 5 points!");
+}
+else{
+    console.log('player just earned 10 points!');
+}
