1f80bd8ab3cac98398d7ad2d5eb4547d1f682c2c
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:49:03 -0700

    Add files via upload

---
+++ 36_t_shirts.ts
@@ -0,0 +1,12 @@
+// writting a function named make shirt with parametrs
+// function should print some text on shirt summerizing the size
+// call the function
+
+function make_shirt(size:string,text:string){
+    console.log('making a shirt of size '+size+ ' and printing '+text + ' on it!')};
+make_shirt('small','star'); 
+
+make_shirt("medium","palnet");
+
+make_shirt("large","glaxy");
+
