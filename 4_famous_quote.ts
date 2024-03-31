38ea06d21fb92ad654538fc60af2484af875780d
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:18:54 -0700

    Add files via upload

---
+++ 4_famous_quote.ts
@@ -0,0 +1,7 @@
+// quote from a famous person and printing t in qoutes along with auther name
+
+let person:string='Quaid-e-Azam ';
+let quote:string='"I do not take right decision,I take decision and make them right!".'
+
+
+console.log (person+'once said, '+ quote);
