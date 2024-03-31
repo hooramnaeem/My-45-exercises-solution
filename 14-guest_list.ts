803271420032b801429cd74735bb1be826951191
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:47:19 -0700

    Add files via upload

---
+++ 14-guest_list.ts
@@ -0,0 +1,8 @@
+// making a list of three persons to invite to dinner and then printing message to each person
+
+let guestList:string[]=['huma','uzma','maria'];
+
+for(let i=0;i<guestList.length; i++){
+    console.log('Dear ' +guestList[i]+ ' I would love to have you at dinner tonight!')
+};
+
