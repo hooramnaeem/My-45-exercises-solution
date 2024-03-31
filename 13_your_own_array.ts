1eeb4d790e037a9872456abab7b199ec456408df
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:45:42 -0700

    Add files via upload

---
+++ 13_your_own_array.ts
@@ -0,0 +1,10 @@
+// making a list of favourite mode of transportation and printing diifrent message 
+
+let transportation:string[]=['motorcycle.','car.','train.','bus.',];
+
+for(let i=0;i<transportation.length; i++){
+    console.log('i always love to travel by '+transportation[i])
+};
+
+
+
