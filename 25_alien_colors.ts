47f596957e92eb4493a19f0945c40afce0ea36fd
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:12:37 -0700

    Add files via upload

---
+++ 25_alien_colors.ts
@@ -0,0 +1,15 @@
+// creating a varieble 
+      let alien_color:string='green';
+
+    // if alien color is green ,print a message that player just earned 5 points
+if(alien_color==='green'){
+    console.log("player just earned 5 points!");
+}
+// if alien color is not green,that failed version will not have any out put
+alien_color='red';
+if(alien_color==='green'){
+    console.log("player just earned 5 points!");
+}
+
+
+
