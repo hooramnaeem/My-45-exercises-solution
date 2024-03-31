5bd7890a5ac953e7d12717bc093227cb85424d46
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:06:15 -0700

    Add files via upload

---
+++ 22_intentional_error.ts
@@ -0,0 +1,8 @@
+// creating an intentional  index error in an array
+let anArray:string[]=['red','blue','green','pink'];
+
+// printing array by index
+console.log(anArray[4]);
+
+// correcting that error
+console.log(anArray); 
