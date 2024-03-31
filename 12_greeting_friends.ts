14c6e61a6f114f55265ee8a5f40ac3ff8bd199dc
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:43:58 -0700

    Add files via upload

---
+++ 12_greeting_friends.ts
@@ -0,0 +1,7 @@
+let friends:string[]=['adan','asad','ali','aahil'];
+
+let message:string="  Hy,how are you?";
+
+for(let i=0; i<friends.length; i++){
+    console.log(friends  [i]   +  message);
+}
