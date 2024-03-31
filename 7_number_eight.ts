a7ac5270706967360b9bd769eb528a032c1a5f6c
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:26:20 -0700

    Add files via upload

---
+++ 7_number_eight.ts
@@ -0,0 +1,12 @@
+// writting addition,subtraction ,multiplication,division operations each resulting in number 8 
+let a:number=4+4
+console.log("addition(4+4) = " +a);
+
+let b:number=12-4
+console.log("subtraction(12-4) = "+b);
+
+let c:number=4*2
+console.log("multiplication(4*2) = "+c);
+
+let d:number=56/7
+console.log("division(56/7) = "+d);
