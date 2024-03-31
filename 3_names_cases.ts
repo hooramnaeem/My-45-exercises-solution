097bb615c9b52011e8cd6b236870b73869b70c20
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:17:27 -0700

    Add files via upload

---
+++ 3_names_cases.ts
@@ -0,0 +1,13 @@
+// store a person name in avariable and print that name in lower ,upper and title cases
+let personName:string='adan';
+
+// lowercase
+console.log("lowercase " +personName.toLowerCase());
+
+// uppercase
+console.log("uppercase " +personName.toUpperCase());
+
+// titlecase
+console.log("titlecase "+personName.charAt(0).toUpperCase()+personName.slice(1,4));
+
+
