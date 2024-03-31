67214a425673eb5b796f127662e029fd86c6a7fd
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:08:03 -0700

    Add files via upload

---
+++ 23_conditional_tests.ts
@@ -0,0 +1,47 @@
+// writing series of conditional tests and printing a statment to describe our prediction
+// creating a varible 
+let car='subaru';
+
+// performing five true  conditional test
+// test # 1
+console.log('is car==="subaru"? i predict true!');
+console.log(car==='subaru');
+
+// test #2
+console.log('is car=="subaru"? my prediction is true.');
+console.log(car=='subaru');
+
+// test#3
+console.log('is car !=="cultus"? my prediction is true.');
+console.log(car!=='honda');
+
+// test#4
+console.log('is car.upper case =="SUBARU"? my prediction is true.');
+console.log(car.toUpperCase()=='SUBARU');
+
+// test#5
+console.log('is car.to lowercase=="subaru"?my prediction is true.');
+console.log(car.toLowerCase()=="subaru");
+
+// now 5 false conditional tests
+// test#1
+console.log('is car ===suzuki? i predict false.');
+console.log(car=='suzuki');
+
+// test#2
+console.log('is car===!subaru? i predict false.');
+console.log(car!=='subaru');
+
+// test#3
+console.log('is car ===SUBARU? i predict false.');
+console.log(car==='SUBARU');
+
+// test#4
+console.log('is car !==subaru?i predict false.');
+console.log(car!=='subaru');
+
+// test#5
+console.log('is car ==bus? i predict false.');
+console.log(car=='bus');
+
+
