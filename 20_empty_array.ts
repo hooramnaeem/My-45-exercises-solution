404c0f02928e3d845868319a84efc4a5d88c7c09
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:01:30 -0700

    Add files via upload

---
+++ 20_empty_array.ts
@@ -0,0 +1,12 @@
+// making a list of somthing stored in array writting a programme containing these items
+// list of rivers
+let rivers:string[]=['Indus ','Jehlum ','ravi ',];
+
+// writting a programe that creates a list containig these items
+let rivers2:string[]=[];
+
+rivers2.push("Niel");
+rivers2.push("Amazon");
+rivers2.push("Dajla");
+
+console.log(rivers2);
