abee4a17732e2fb0e33d1e74df4e5ee1e239295f
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:31:59 -0700

    Add files via upload

---
+++ 9_favourite_number.ts
@@ -0,0 +1,5 @@
+// storing my favourit number in a variable and priting a message revealin that favourite number
+
+let favouriteNumber:number=7;
+console.log(favouriteNumber+" is always be a lucky number for me that is why it is my favourite number.");
+
