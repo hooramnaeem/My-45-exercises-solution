ac53592b864d5fbf138062baea041c8a5a83546f
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:41:20 -0700

    Add files via upload

---
+++ 29_favourite_fruit.ts
@@ -0,0 +1,23 @@
+// making an array of favourit fruites
+// writting five if statment ,to check the fruit is in our array if it is if block should print a statment
+let favourit_fruit:string[]=['apple','mango','banana'];
+ 
+if(favourit_fruit.includes('apple')){
+    console.log('i really like banana!')
+}
+if(favourit_fruit.includes('mango')){
+    console.log('i really like banana!')
+}
+if(favourit_fruit.includes('banana')){
+    console.log('i really like banana!')
+}
+if(favourit_fruit.includes('pear')){
+    console.log('i really like banana!')
+}
+if(favourit_fruit.includes('peach')){
+    console.log('i really like banana!')
+}
+
+
+
+
