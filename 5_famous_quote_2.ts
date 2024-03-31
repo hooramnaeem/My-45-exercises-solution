b21e49ce8c6bb1ef6ec30f1c698ce43286eee510
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 10:21:20 -0700

    Add files via upload

---
+++ 5_famous_quote_2.ts
@@ -0,0 +1,8 @@
+// repeat exec=rcise #4
+//storing person name andquote in a variable and then printing it
+
+let famousPerson:string='Quaid-e-Azam';
+
+let message:string=famousPerson+' once said," I do not take right decision,I take decision and make them right!"';
+  
+console.log(message);
