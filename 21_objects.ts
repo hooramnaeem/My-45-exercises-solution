28eff186bd96f99bcbe7e3e2c065674080ce8233
hooramnaeem <163731775+hooramnaeem@users.noreply.github.com>
2024-03-31 11:04:19 -0700

    Add files via upload

---
+++ 21_objects.ts
@@ -0,0 +1,49 @@
+// creating first object
+interface person{
+    Name:string,
+    Qualification:string,
+    Age:number,
+    Maritalstatus:boolean
+}
+let person:person={
+    Name:'adan',
+    Qualification:'Msc',
+    Age:25,
+    Maritalstatus:false
+}
+console.log(person);
+
+interface book{
+    Name:string,
+    Edition:number,
+    price:string                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+}
+let book:book={
+    Name:'basics of type script',
+    Edition:2022,
+    price:'Rs.1000',
+}
+console.log(book);
+
+// creating third object
+interface flower{
+    Name:string,
+    color:string,
+    Availability:boolean
+}
+let flower:flower={
+    Name:'Rose',
+    color:'red',
+    Availability:true
+}
+console.log(flower);
+
+
+
+
+
+
+
+
+
+
